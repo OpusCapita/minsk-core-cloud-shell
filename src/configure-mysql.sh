8c7e968261d3b825a28f42522d039567c7925de7
Egor Stambakio <egor.stambakio@opuscapita.com>
2020-02-27 10:43:46 +0300

    Fix mysql settings

--- src/configure-mysql.sh
+++ src/configure-mysql.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-settings_file="${HOME}/.local/share/code-server/User/settings.json"
+settings_file="${HOME}/.local/share/code-server/User/state/global.json"
 
 echo "[INFO] Write MySQL connection data to ${settings_file}"
 
@@ -31,21 +31,29 @@ tmp=$(mktemp)
 if test -f ${settings_file}; then
   cat ${settings_file} > $tmp
 else
-  echo '{}' > $tmp
+  echo '[]' > $tmp
 fi
 
 mkdir -p $(dirname ${settings_file})
 
-cat $tmp | jq \
-  --arg server "${server}" \
-  --arg database "${database}" \
-  --arg user "${user}" \
-  --arg password "${password}" '.["mysql.connections"]=[
-    {
-      "host": $server,
-      "database": $database,
-      "user": $user,
-      "password": $password,
-      "port": 3306
-    }
-  ]' > ${settings_file}
+connections=$(
+  jq -nc --arg server "${server}" \
+    --arg database "${database}" \
+    --arg user "${user}" \
+    --arg password "${password}" \
+    '.["mysql.connections"]={
+      "552d6ff0-5923-11ea-b914-85d617ff0ebb": {
+        "host": $server,
+        "database": $database,
+        "user": $user,
+        "password": $password,
+        "port": 3306,
+        "certPath": ""
+      }
+    }'
+)
+
+cat $tmp | jq --arg connections "${connections}" '.+=([[
+    "formulahendry.vscode-mysql",
+    $connections
+  ]])' > ${settings_file}
