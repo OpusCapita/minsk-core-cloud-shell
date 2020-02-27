462d15733a01fd84d309c67517c204f39ecc840a
Egor Stambakio <egor.stambakio@opuscapita.com>
2020-02-27 08:54:51 +0300

    Try

---
+++ src/configure-mysql.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+settings_file="${HOME}/.local/share/code-server/User/settings.json"
+
+echo "[INFO] Write MySQL connection data to ${settings_file}"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --server=*) server="${1#*=}" ;;
+    --database=*) database="${1#*=}" ;;
+    --user=*) user="${1#*=}" ;;
+    --password=*) password="${1#*=}" ;;
+    *)
+      echo "$(basename $0) ERROR: invalid argument ${1}"
+      exit 1
+  esac
+  shift
+done
+
+for v in server database user password; do
+  if [[ -z "${!v}" ]]; then
+    printf "$(basename $0): ERROR: $v is required but empty\n"
+    exit 1
+  fi
+done
+
+tmp=$(mktemp)
+
+if test -f ${settings_file}; then
+  cat ${settings_file} > $tmp
+else
+  echo '{}' > $tmp
+fi
+
+mkdir -p $(dirname ${settings_file})
+
+cat $tmp | jq \
+  --arg server "${server}" \
+  --arg database "${database}" \
+  --arg user "${user}" \
+  --arg password "${password}" '.["mysql.connections"]=[
+    {
+      "host": $server,
+      "database": $database,
+      "user": $user,
+      "password": $password,
+      "port": 3306
+    }
+  ]' > ${settings_file}
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -11,6 +11,13 @@ while [ $# -gt 0 ]; do
     --oc-mssql-database=*) mssql_database="${1#*=}" ;;
     --oc-mssql-user=*) mssql_user="${1#*=}" ;;
     --oc-mssql-password=*) mssql_password="${1#*=}" ;;
+
+    --oc-enable-mysql=*) enable_mysql="${1#*=}" ;;
+    --oc-mysql-server=*) mysql_server="${1#*=}" ;;
+    --oc-mysql-database=*) mysql_database="${1#*=}" ;;
+    --oc-mysql-user=*) mysql_user="${1#*=}" ;;
+    --oc-mysql-password=*) mysql_password="${1#*=}" ;;
+
     *)
       rest_args+=("$1")
   esac
@@ -24,6 +31,10 @@ fi
 
 if [[ $enable_mssql = true ]]; then
   /configure-mssql.sh --server="${mssql_server}" --database="${mssql_database}" --user="${mssql_user}" --password="${mssql_password}"
+fi
+
+if [[ $enable_mysql = true ]]; then
+  /configure-mysql.sh --server="${mysql_server}" --database="${mysql_database}" --user="${mysql_user}" --password="${mysql_password}"
 fi
 
 # Run code-server
